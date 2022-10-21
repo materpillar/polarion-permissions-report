4e75475def8edb6498315a8c1ab3fc270692e4af
Matthias Treber <>
2022-10-21 18:59:00 +0200

    Initial working version

---
+++ README.md
@@ -0,0 +1,12 @@
+# Polarion Permissions HTML Report
+
+creates a HTML table of Polarion roles' permissions
+based on the global and project permissions XML files.
+
+Thanks to Sebastiaan Langeveld for the prep work.
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,76 +1,63 @@
-# 1. Assuming that project_admin has all rights.
-# 2. Create table headers with all permission
-# 3. loop trough all the non-project_admin roles, and then fill in table.
+#!/usr/bin/env python3
+
 from jinja2 import Environment, FileSystemLoader
 import xml.etree.ElementTree as ET
 
 
-def gather_headers():
-    all_headers = []
-    print('Gathering all headers')
+def get_all_permissions(roles):
+    permissions = []
     tree = ET.parse('input.xml')
     root = tree.getroot()
 
-    roles = root.find(".//role[@name='project_admin']")
-    rules = roles.getchildren()
-    for rule in rules:
-        all_headers.append(rule.attrib['permission'])
+    for role in roles:   
+        role_element = root.find(".//role[@name='" + role +"']")
+        grants_denies = list(role_element)
+        for grant_deny in grants_denies:
+            if grant_deny.attrib['permission'].replace("com.polarion.","") not in permissions:
+                permissions.append(grant_deny.attrib['permission'].replace("com.polarion.",""))
 
-    all_clean_headers = clean_up_headers(all_headers)
+    permissions.sort()
+    return permissions
 
-    return all_clean_headers
 
-def gather_categories(clean_headers):
-    header_categories = []
-    for unchecked_header in clean_headers:
-        category = unchecked_header.split('.')[0]
-        if category not in header_categories:
-            header_categories.append(category)
+def gather_data(roles, permissions):
+    permission_dict = dict.fromkeys(permissions)
+    for permission in permissions:
+        permission_dict[permission] = dict.fromkeys(roles)
 
-    return header_categories
-
-def clean_up_headers(headers):
-    clean_headers = []
-    for header in headers:
-        clean_headers.append(header.replace("com.polarion.",""))
-
-    return clean_headers
-
-def gather_data():
-    print('Converting XML to HTML Table...')
     tree = ET.parse('input.xml')
     root = tree.getroot()
+    for role in roles:   
+        grants_denies = list(root.find(".//role[@name='" + role +"']"))
+        for grant_deny in grants_denies:
+            permission = grant_deny.attrib['permission'].replace("com.polarion.","")
+            if grant_deny.tag == "deny":
+                permission_dict[permission][role] = "\N{cross mark}"
+            elif grant_deny.tag == "grant":
+                permission_dict[permission][role] = "\N{check mark}"
 
-    data = []
-    for elems in root:
-        newFood = {}
-        for food in elems: 
-            tag = food.tag
-            text = food.text
-            newFood[tapersistence.object.TestRun.deleteg] = text
-        
-        data.append(newFood)
     
-    return data
+    return permission_dict
 
-def generate_html(headers, data):
+def generate_html(roles, permissions, permission_dict):
     enviroment = Environment(loader=FileSystemLoader("templates"))
 
     results_filename = "data_results.html"
     results_template = enviroment.get_template("results.html")
     context = {
-        "headers":headers,
-        "datas": data
+        "permissions": permissions,
+        "roles": roles,
+        "permission_dict": permission_dict
     }
     with open(results_filename, mode="w", encoding="utf-8") as results:
         results.write(results_template.render(context))
         print(f"Job done..")
         
 def main():
-    headers = gather_headers()
-    categories = gather_categories(headers)
-    data = gather_data()
-    generate_html(headers, data)
+    roles = ['project_admin', 'project_approver', 'project_user', 'project_assignable']
+    permissions = get_all_permissions(roles)
+    data = gather_data(roles, permissions)
+    generate_html(roles, permissions, data)
 
 if __name__ == "__main__":
     main()
